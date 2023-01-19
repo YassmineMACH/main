139ab492de8eec793b59a6c5e698af5cf392d1dc
YassmineMACH <119419622+YassmineMACH@users.noreply.github.com>
2023-01-19 14:37:43 +0100

    Add files via upload
    Add files

---
+++ src/reducer.js
@@ -0,0 +1,41 @@
+// reducer.js
+import { INPUT, EVAL , RESET } from "./actions";
+
+const initialState = {
+  input: "",
+  result: "",
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case INPUT:
+      return {
+        ...state,
+        input: state.input + action.payload,
+      };
+    case EVAL:
+      try {
+        return {
+          ...state,
+          result: eval(state.input),
+        };
+      } catch (error) {
+        return {
+          ...state,
+          result: "Error",
+        };
+      }
+    // case RESULT:
+    //   return {
+    //     ...state,
+    //     result: action.payload,
+    //   };
+    case RESET:
+      return {
+        input: "",
+        result: "",
+      };
+    default:
+      return state;
+  }
+}
